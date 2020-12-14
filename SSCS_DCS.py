8329a2c4b90b84b0be1f293f767932a2191ea8d4
JeffreyBarrick <jeffrey.e.barrick@gmail.com>
2020-12-14 17:21:17 -0600

    Normalized spaces/tabs

--- SSCS_DCS.py
+++ SSCS_DCS.py
@@ -3,29 +3,29 @@
 """
 Created on Wed Oct  9 10:52:06 2013
 Duplex Seq Input To Breseq
-Step 1 
+Step 1
     Identify Molecular Index (MI) on each read pair
-Step 2 
+Step 2
     Output reads to 1 of 3 intermediate files: Double_MI.fastq, Single_MI.fastq, Zero_MI_pair.fastq. To be based on how many correct MI are identified in the pair.
     MI should be removed from read sequence string, and appended to header
     Quality score should be removed from MI regions
-****Future Improvment: 
+****Future Improvment:
         be more liberal with MI and constant region (ie 11bp then 4bp constant for synthesis error). Need to let non-appropriate reads drive this.
         Quality score of MI region should be used to calculate MI
-Step 3 
+Step 3
     Focus only on Double_MI.fastq first
-    Store tuples in dictionary as 
-    
-    
+    Store tuples in dictionary as
+
+
 #Breseq does not make mutation calls based on "N" sequences. Therefore it is not necessary to identify 1st round PCR errors. #### NOT TRUE when coverag uneven
-    
-    
+
+
 would be nice to add something for reading in gd files ot generate command file for better merge with breseq
 
 current stuff printing to screen should default to printing to log file (specifie from argrse)
 function use of main()
 deletion of intermediate file dictioaries afterwrite
-append stats of run (ie histogram of reads per MI, total reduction, number of Ns, etc) possibly multiple files, 
+append stats of run (ie histogram of reads per MI, total reduction, number of Ns, etc) possibly multiple files,
 
 
 if multiple reads from a single sample:
@@ -91,28 +91,28 @@ def de_duplex_read(header1, read1, quality1, header2, read2, quality2):
             header1 = header1+"#MI:"+read1[:12]
             read1 = read1[17:]
             quality1 = quality1[17:]
-    
+
             header2 = header2+"#MI:"+read2[:12]
             read2 = read2[17:]
-            quality2 = quality2[17:]        
+            quality2 = quality2[17:]
 
             good_reads+=2
             group_assignment(header1, read1, header2, read2) #quality1, quality2 #moved outside of function as quality ends up too high to use
-            
+
             fastq_dict["Double_MI.fastq"].extend([[header1, read1, "+", quality1],[header2, read2, "+", quality2]])
-    
+
         elif re.match(molecular_index_search_string, read1):
             header1 = header1+"#MI:"+read1[:12]
             read1 = read1[17:]
             quality1 = quality1[17:]
             fastq_dict["Single_MI.fastq"].extend([[header1, read1, "+", quality1],[header2, read2, "+", quality2]])
-    
+
         elif re.match(molecular_index_search_string, read2):
             header2 = header2+"#MI:"+read2[:12]
             read2 = read2[17:]
-            quality2 = quality2[17:]          
+            quality2 = quality2[17:]
             fastq_dict["Single_MI.fastq"].extend([[header1, read1, "+", quality1],[header2, read2, "+", quality2]])
-    
+
         else:
             fastq_dict["Zero_MI.fastq"].extend([[header1, read1, "+", quality1],[header2, read2, "+", quality2]])
 
@@ -121,21 +121,21 @@ def de_duplex_read(header1, read1, quality1, header2, read2, quality2):
             header1 = header1+"#MI:"+read1[:12]
             read1 = read1[17:]
             quality1 = quality1[17:]
-    
+
             header2 = header2+"#MI:"+read2[:12]
             read2 = read2[17:]
-            quality2 = quality2[17:]        
+            quality2 = quality2[17:]
 
             good_reads+=2
             group_assignment(header1, read1, header2, read2) #quality1, quality2 #moved outside of function as quality ends up too high to use
-        
+
 def group_assignment(header1, read1, header2, read2): #quality1, quality2 #moved outside of function as quality ends up too high to use
     """function designed to take in fastq information where MI has been added to header, and group reads with identical MI for SingleStrandConensusGroups
     Currently doing nothing with full header or quality score, thisshouldprobably be upgraded to do both things"""
 
     R1_tag = header1.split('#MI:')[-1]+header2.split('#MI:')[-1]
     R2_tag = header2.split('#MI:')[-1]+header1.split('#MI:')[-1]
-   
+
     for tag in [R1_tag, R2_tag]:
         if tag not in master_dict:
             master_dict[tag] = {}
@@ -146,10 +146,10 @@ def group_assignment(header1, read1, header2, read2): #quality1, quality2 #moved
             master_dict[tag]["DCS"] = []
     master_dict[R1_tag]["Read1"].append(read1)
     master_dict[R2_tag]["Read2"].append(read2)
-        
-        
+
+
 def consensus_generation(list_of_sequences, cutoff, read_length):
-    """take in list of sequences, and consensus sequence abve threshold. 
+    """take in list of sequences, and consensus sequence abve threshold.
     Largely copied from Scott Kennedy's consensusMaker.py script used by Schmitt et all
     partially copied from Scott Kennedy's script
     ok, not really copied from tht script anymore"""
@@ -159,7 +159,7 @@ def consensus_generation(list_of_sequences, cutoff, read_length):
     consensus_read = []
     if len(list_of_sequences) not in Base_discrepancies:
         Base_discrepancies[len(list_of_sequences)] = {}
-    
+
     for i in xrange(read_length) : #Count the types of nucleotides at a position in a read. i is the nucleotide index within a read in list_of_sequences
         consensus_dict = {'A':0, 'C':0, 'T':0, 'G':0, 'N':0} #set dictionary  to empty for each base within the readgroup
         if i not in Base_discrepancies[len(list_of_sequences)]:
@@ -170,7 +170,7 @@ def consensus_generation(list_of_sequences, cutoff, read_length):
             consensus_dict[list_of_sequences[j][i]] +=1.0
 
         #add an assertion for the sum total of base counts equal to the number of sequences?
-        if consensus_dict[max(consensus_dict.iterkeys(), key=(lambda key: consensus_dict[key]))]/float(len(list_of_sequences)) >cutoff: #this would fall apart if a cutoff of less than 0.5 is used     
+        if consensus_dict[max(consensus_dict.iterkeys(), key=(lambda key: consensus_dict[key]))]/float(len(list_of_sequences)) >cutoff: #this would fall apart if a cutoff of less than 0.5 is used
             consensus_read.append(max(consensus_dict.iterkeys(), key=(lambda key: consensus_dict[key])))
             Base_discrepancies[len(list_of_sequences)][i].append(consensus_dict[max(consensus_dict.iterkeys(), key=(lambda key: consensus_dict[key]))]/float(len(list_of_sequences)))
             if Base_discrepancies[len(list_of_sequences)][i][-1] != 1.0 and len(list_of_sequences) ==2:
@@ -184,8 +184,8 @@ def consensus_generation(list_of_sequences, cutoff, read_length):
             Unresolved_consensus[i] += 1
     consensus_read = "".join(consensus_read)
     return consensus_read
-    
-#read fastq1 an fastq2 in together        
+
+#read fastq1 an fastq2 in together
 line_count = 0
 with open(args.fastq1, "r") as Fastq1, open(args.fastq2, "r") as Fastq2:
     for line1 in Fastq1:
@@ -199,8 +199,8 @@ with open(args.fastq1, "r") as Fastq1, open(args.fastq2, "r") as Fastq2:
         elif line_count%4 ==2:#line = read
             read1 = line1
             read2 = line2
-            assert re.match('^[ACTGN]*$', read1) and re.match('^[ACTGN]*$', read2), "Non-sequence characters found on one of th following sequence lines:\n%s\n%s" %(line1,line2) 
-        elif line_count%4 ==3: #line = optional sequence descriptor. assume line to use 'standard' "+" notation rather than actual descriptor. 
+            assert re.match('^[ACTGN]*$', read1) and re.match('^[ACTGN]*$', read2), "Non-sequence characters found on one of th following sequence lines:\n%s\n%s" %(line1,line2)
+        elif line_count%4 ==3: #line = optional sequence descriptor. assume line to use 'standard' "+" notation rather than actual descriptor.
             assert line1 == line2 == "+", "line1 or line2 does not display expected '+' sign on line3 here:\nline1: %s\nline2: %s" %(line1, line2)
         elif line_count%4 ==0:
             quality1 = line1
@@ -208,10 +208,10 @@ with open(args.fastq1, "r") as Fastq1, open(args.fastq2, "r") as Fastq2:
             de_duplex_read(header1, read1, quality1, header2, read2, quality2)
         if line_count%200000 == 0:
             print>>log, line_count/4, "reads processed"
-           # break #for testing subsetof reads rather than full read list                    
+           # break #for testing subsetof reads rather than full read list
 sys.stdout.flush() #dump processed reads out before continuing
 
-##### write 3 fastq files from fastq_dict serves as early intermediate of where to focus on future improvements            
+##### write 3 fastq files from fastq_dict serves as early intermediate of where to focus on future improvements
 if args.intermediate_fastqs:
     for entry in fastq_dict:
         output_name = args.prefix+entry
@@ -219,7 +219,7 @@ if args.intermediate_fastqs:
             for read in fastq_dict[entry]:
                 print>>output, "\n".join(read)
 
-#generte SSCS and DCS consensus sequences 
+#generte SSCS and DCS consensus sequences
 Reads_per_MI_Dict = defaultdict(int) #usedefault dict to simplif next 6 lines
 for MI in master_dict:
 #    if len(master_dict[MI]["Read1"]) not in Reads_per_MI_Dict:
@@ -228,7 +228,7 @@ for MI in master_dict:
 #        Reads_per_MI_Dict[len(master_dict[MI]["Read2"])] = 0
     Reads_per_MI_Dict[len(master_dict[MI]["Read1"])] +=1
     Reads_per_MI_Dict[len(master_dict[MI]["Read2"])] +=1
-    
+
     if args.SSCS or args.DCS:
         if len(master_dict[MI]["Read1"]) > (args.minimum_reads-1):
             assert len(master_dict[MI]["SSCS1"]) == 0, "duplicated MI: %s" %MI
@@ -260,10 +260,10 @@ with open(args.prefix+"_DCS.fastq", "w") as DCS, open(args.prefix+"_SSCS.fastq",
             single_count +=1
         if len(master_dict[MI]["Read2"]) == 1: # if read2 only has 1read thn no sscs or dcs redcan exist
             print>>singles, "\n".join(map(str, ["@"+str(MI)+"-"+str(single_count), master_dict[MI]["Read2"][0], "+", '+'*len(master_dict[MI]["Read2"][0])])) #+ = quality score of10
-            single_count +=1        
+            single_count +=1
         if len(master_dict[MI]["Read1"]) == 1 and len(master_dict[MI]["Read2"]) == 1:
             #doube singletons included within singleton files, if going include this it needsto be done durring consensus sequence gen
-            double_singletons += 1    
+            double_singletons += 1
 
 ########Reporting information
 
@@ -273,7 +273,7 @@ elapsed = end - start
 print>>log, "Histogram of number of reads per MI."
 for key in sorted(Reads_per_MI_Dict.keys()):
     print>>log, key, "\t", Reads_per_MI_Dict[key]
-    
+
 print>>log, "Accuracy info divided by number MI read depth. Rows = Read Depth, Columns = base position"
 print>>log, "\t".join(map(str, ["Read Depth"]+list(sorted(base_positions))))
 for read_Depth in Base_discrepancies:
@@ -286,7 +286,7 @@ for read_Depth in Base_discrepancies:
 print>>log, "Histogram info on number of discrepent bases:"
 print>>log, "\t".join(map(str, ["Read Position", "Unresolved Consensus Seq", "total Consesnsus calls"]))
 #for entry in sorted(Unresolved_consensus.keys()):
-for position in list(sorted(base_positions)): 
+for position in list(sorted(base_positions)):
     print>>log, position, "\t", Unresolved_consensus[position], "\t", (total_concensus_attempts - Unresolved_consensus[position])
 
 print>>log, "total time elapsed:\t%s" %elapsed
